6e252a4b325e89f2a6bff9872c5c22de13da5212
Caio Paiva <Caio Paiva>
2016-11-17 11:31:45 -0500

    Remove File recovery content

---
+++ https2.0/https.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+const yargs = require('yargs');
+const http = require('./http');
+const pathUtil = require('path');
+
+const argv = yargs.usage('node https.js [-v verbose][-p port] [-d Path-to-directory]')
+    .default('p', 8080)
+    .default('d', ".")
+    .default('v', false)
+    .help('help')
+    .argv;
+
+function Logger(){
+    Logger.prototype.log = (!argv.v ? function(){} : function(message){
+        console.log(message);
+    });
+};
+var logger = new Logger();
+
+var httpServer = new http(argv.p,
+    // GET Handler
+    function(socket, request){
+        var path = pathUtil.normalize('./'+request.split(' ')[1]);
+        //   Can't resquest files outside directory'
+        if(validPath(path)){
+            var stat;
+            try{
+                stat = fs.statSync(argv.d+'/'+path);
+            }catch(err){
+                logger.log("Request not found: Sending 404");
+                return this.sendReply(socket, "404");
+            }
+            if(stat){
+                var content;
+                if(stat.isFile())
+                    content = fs.readFileSync(path).toString();
+                else if(stat.isDirectory())
+                    content = fs.readdirSync(path);
+                return this.sendReply(socket, "200", content, path);
+            }else{
+                logger.log("Request not found: Sending 404");
+                return this.sendReply(socket, "404");
+            }
+        }else{
+            logger.log("Client trying to access invalid location: Sending 403");
+            return this.sendReply(socket, "403");
+        }
+    },
+    // POST Handler
+    function(socket, request){
+        var path = pathUtil.normalize('/.'+request.split(' ')[1]);
+        if(validPath(path)){
+            var body = request.split('\r\n\r\n')[1];
+            fs.writeFileSync(argv.d+'/'+path, body);
+            var tmp = path.split('/');
+            var pathDir = tmp.slice(0, tmp.length-1).join('/');
+            console.log(pathDir);
+            var dir = fs.readdirSync(pathUtil.resolve(argv.d+'/'+path, '..'));
+            return this.sendReply(socket, "201", dir);
+        }else{
+            return this.sendReply(socket, "403");
+        }
+    }, logger);
+
+    function validPath(path){
+        console.log(path)
+        return (path.includes("..") ? false : true);
+    }
--- saveme.py
+++
@@ -1,8 +0,0 @@
-import os
-
-lines = [line.rstrip('\n') for line in open('blob')]
-
-for val in lines:
-    os.system('git cat-file -p '+val)
-    os.system("pause")
-
