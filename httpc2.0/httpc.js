99f7dcc88a74115ca3639ffa89b6acf377f8d414
Caio <cclp94@gmail.com>
2016-11-27 01:43:49 -0500

    Changes for big replies

--- httpc2.0/httpc.js
+++ httpc2.0/httpc.js
@@ -11,6 +11,9 @@ const fs = require('fs');
 const router = 'http://localhost:3000';
 const WINDOW_SIZE = 10;
 
+var sequenceFollower = 0;
+var queue = [];
+
 
 var window_buffer = [];
 var sendingQueue = [];
@@ -89,56 +92,70 @@ const argv = yargs.usage('Usage: node $0 (get|post) [-v] (-h "k:v")* [-d inline-
             port: 8000,
             exclusive: true
         }, () =>{
-            var packet = (new Packet())
+            var firstPacket = (new Packet())
                     .setType(0)
                     .setSequenceNumber(0)
                     .setAddress(parsedURL.hostname)
                     .setPort(parsedURL.port)
                     .setPayload('jhkjhk');
-            addToSendingQueue(packet);
+            addToSendingQueue(firstPacket);
             var connection = false;
             socket.on('message', (msg, rinfo) => {
-                var ackSynPk = Packet.createFromBuffer(msg);
-                console.log(`client got: ${ackSynPk.sequenceNumber} type ${ackSynPk.type}`);
-                if(ackSynPk.type == 1){             // SYN + ACK
+                var packet = Packet.createFromBuffer(msg);
+                console.log(`client got: ${packet.sequenceNumber} type ${packet.type}`);
+                if(packet.type == 1){             // SYN + ACK
                     connection = true;
-                    removeFromWindow(ackSynPk.sequenceNumber);
-                    ackSynPk = ackSynPk.setType(2)
+                    removeFromWindow(packet.sequenceNumber);
+                    packet = packet.setType(2)
                                     .setAddress(parsedURL.hostname)
                                     .setPort(parsedURL.port);
-                    addToSendingQueue(ackSynPk);
+                    addToSendingQueue(packet);
                     // Connection estabilished
-                    var seq = startTransmittion(httpRequest, ackSynPk);
+                    var seq = startTransmittion(httpRequest, packet);
                     // Send Close connection signal
                     var FINPk = packet.copy()
                                     .setType(4)
                                     .setSequenceNumber(seq)
                                     .setPayload('');
                     addToSendingQueue(FINPk);
-
-                }else if(ackSynPk.type == 2 && connection){           // ACK
+                    sequenceFollower = seq +1;
+                }else if(packet.type == 2 && connection){           // ACK
                     // Pop Cumulative ack from window
-                    if(ackSynPk.sequenceNumber > 0)
-                        removeFromWindow(ackSynPk.sequenceNumber);
-                }else if(ackSynPk.type == 4 && connection){           // FIN + ACK
-                    removeFromWindow(ackSynPk.sequenceNumber);
-                    console.log(ackSynPk.payload);
-                    console.log("Sending last");
-                    var last = ackSynPk.copy()
-                                    .setType(2)
-                                    .setPayload('');
-                    // Send and don't add to window
-                    sendPacket(last, router);
+                    removeFromWindow(packet.sequenceNumber);
+                }else if(packet.type == 3 && connection){      
+                    if (packet.sequenceNumber == sequenceFollower)
+                        addToQueue(packet, true, this);
+                    else if (packet.sequenceNumber > sequenceFollower)
+                        addToQueue(packet, false);
+                    sendAcknowledgement(packet);
+                }else if(packet.type == 4 && connection){           // FIN + ACK
+                    removeFromWindow(packet.sequenceNumber);
+
+                    if (packet.sequenceNumber > sequenceFollower &&
+                        packet.sequenceNumber !== sequenceFollower)
+                        addToQueue(packet, false);
+                    sendAcknowledgement(packet);
+                    if (packet.sequenceNumber === sequenceFollower) {
+                        deliverQueue();
+                    }
                     setTimeout(function(){
                         socket.close();
                     }, 5000 );
                     return;
-                }else if(ackSynPk.type == 5 && connection){           // NACK
-                    retransmit(ackSynPk.sequenceNumber);
-                    console.log(ackSynPk.payload);
+                }else if(packet.type == 5 && connection){           // NACK
+                    retransmit(packet.sequenceNumber);
+                    console.log(packet.payload);
                 }
             });
         });
+    }
+
+    function sendAcknowledgement(oldPk) {
+        var packet = oldPk.copy()
+            .setType(2)
+            .setSequenceNumber(sequenceFollower)
+            .setPayload('');
+        sendPacket(packet, router);
     }
 
     function retransmit(seq){
@@ -146,28 +163,23 @@ const argv = yargs.usage('Usage: node $0 (get|post) [-v] (-h "k:v")* [-d inline-
         sendPacket(window_buffer[0].packet, router);
     }
 
-    function removeFromWindow(seq){
-        console.log("remove up to: " + seq);
+    function removeFromWindow(arrivedSeq){
+        console.log("remove up to: " + arrivedSeq);
         console.log('window size: ' + window_buffer.length+ 'queue size: '+sendingQueue.length);
-        if(window_buffer.length > 0 && seq < window_buffer[0].seq ) return;
+        if(window_buffer.length > 0 && arrivedSeq < window_buffer[0].seq ) return;
         console.log(window_buffer.reduce((result, cur)=>{return result += ', '+cur.seq}, ''));
-        for(var i in window_buffer){
-            if(window_buffer[i].seq <= seq-1){
-                clearInterval(window_buffer[i].timer);
-                console.log(window_buffer[i].seq + " cleared");
-            }
-            if(window_buffer[i].seq === seq-1){
-                console.log('here');
-                console.log('Before splice: '+window_buffer.reduce((result, cur)=>{return result += ', '+cur.seq}, ''));
-                window_buffer.splice(0, i+1);
-                console.log('Window now: '+window_buffer.reduce((result, cur)=>{return result += ', '+cur.seq}, ''));
-                var toSend = sendingQueue.splice(0 ,i+1);
-                for(var j in toSend){
-                    addToWindowAndSend(toSend[j]);
-                }
-                return;
-            }
+        var i = 0;
+        while(window_buffer.length !== 0 && window_buffer[0].seq <= arrivedSeq-1){
+            clearInterval(window_buffer.shift().timer);
+            i++;
         }
+                
+        console.log(window_buffer.reduce((result, cur)=>{return result += ', '+cur.seq}, ''));
+        var toSend = sendingQueue.splice(0 ,i);
+        for(var j in toSend){
+            addToWindowAndSend(toSend[j]);
+        }
+        return;
     }
 
     function addToWindowAndSend(pk){
@@ -232,6 +244,92 @@ const argv = yargs.usage('Usage: node $0 (get|post) [-v] (-h "k:v")* [-d inline-
         console.log("Sending packet #"+packet.sequenceNumber+ "type: "+packet.type);
         socket.send(buffer, 0, buffer.length, dest.port, dest.hostname);
     }
+
+    // RECEIVER
+    function addToQueue(pk, expected) {
+    if (queue.length === 0 || queue[queue.length - 1].seq < pk.sequenceNumber) {
+        queue.push({
+            seq: pk.sequenceNumber,
+            data: pk.payload,
+            address: pk.address,
+            port: pk.port,
+            isFIN: pk.type === 4 ? true : false
+        });
+        if (expected === true) sequenceFollower++;
+        console.log("1. Queue has: "); console.log(queue.reduce((result, cur) => { return result += ', ' + cur.seq }, ''));
+    } else if (queue[0].seq > pk.sequenceNumber) {
+        queue.unshift({
+            seq: pk.sequenceNumber,
+            data: pk.payload,
+            address: pk.address,
+            port: pk.port,
+            isFIN: pk.type === 4 ? true : false
+        });
+        if (expected === true) {
+            var lastInOrder = sequenceFollower;
+            for (var j = 0; j < queue.length; j++) {
+                var lastInOrder = queue[j].seq;
+                if (!queue[j + 1] || queue[j + 1].seq - queue[j].seq > 1)
+                    break;
+            }
+            sequenceFollower = lastInOrder + 1;
+        }
+        console.log("2. Queue has: "); console.log(queue.reduce((result, cur) => { return result += ', ' + cur.seq }, ''));
+
+    } else {
+
+        for (var i = queue.length - 1; i >= 0; i--) {
+            if (queue[i].seq === pk.sequenceNumber) return;
+            if (queue[i].seq < pk.sequenceNumber) {
+                queue.splice((i + 1), 0, {
+                    seq: pk.sequenceNumber,
+                    data: pk.payload,
+                    address: pk.address,
+                    port: pk.port,
+                    isFIN: pk.type === 4 ? true : false
+                });
+                if (expected === true) {
+                    var lastInOrder = sequenceFollower;
+                    for (var j = (i !== 0 ? i + 1 : 0); j < queue.length; j++) {
+                        var lastInOrder = queue[j].seq;
+                        if (!queue[j + 1] || queue[j + 1].seq - queue[j].seq > 1)
+                            break;
+                    }
+                    sequenceFollower = lastInOrder + 1;
+                }
+                console.log("3. Queue has: "); console.log(queue.reduce((result, cur) => { return result += ', ' + cur.seq }, ''));
+                break;
+            }
+        }
+    }
+    if (expected)
+        checkDelivery();
+}
+
+function checkDelivery() {
+    if (queue[queue.length - 1].isFIN && queue[queue.length - 1].seq === sequenceFollower - 1) {
+        console.log("Send reply");
+        var packet = (new Packet()).setType(2)
+            .setAddress(queue[queue.length - 1].address)
+            .setPort(queue[queue.length - 1].port)
+            .setSequenceNumber(++sequenceFollower)
+            .setPayload('');
+        sendPacket(packet, router);
+        deliverQueue();
+        closingConnection = true;
+    }
+}
+
+function deliverQueue(){
+    var request = queue.reduce((req, pk) => {
+            console.log(pk.data);
+            return req + pk.data;
+        }, '');
+    console.log(request);
+}
+
+
+    // HTTP
 
     function createHTTPMessage(argv, parsedURL){
          if(argv.URL){
